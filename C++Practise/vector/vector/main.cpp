7a17e112ba929e9fd4d73486f6221157bb6a47a7
wzs <251390743@qq.com>
2016-09-05 23:15:32 +0800

    modify main.cpp

--- C++Practise/vector/vector/main.cpp
+++ C++Practise/vector/vector/main.cpp
@@ -2,13 +2,20 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 using namespace std;
 int main(int argc, char **argv)
 {
 	vector<string>::size_type ix = 89;
 	vector<string> svec(ix,"hi");
 	svec[5] = "wzs";
-	cout<<svec[4]<<"......."<<svec.size()<<endl;
+    svec.push_back("woaini");
+   // svec.reserve(200);
+   while(svec.size() != svec.capacity())
+       svec.push_back("9");
+	cout<<"svec.capacity = "<<svec.capacity()<<" ,svec.size = "<<svec.size()<<endl;
+    svec.push_back("100");
+    cout<<"svec.capacity = "<<svec.capacity()<<" ,svec.size = "<<svec.size()<<endl;
 	printf("hello world\n");
 	getchar();
 	return 0;
